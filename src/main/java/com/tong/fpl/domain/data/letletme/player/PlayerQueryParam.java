a05c0f14d20511d43fa1fe33a5892ab231c3758e
tong <bluedragon00000@gmail.com>
2020-08-22 01:52:16 +0800

    api

--- src/main/java/com/tong/fpl/domain/data/letletme/player/PlayerQueryParam.java
+++ src/main/java/com/tong/fpl/domain/data/letletme/player/PlayerQueryParam.java
@@ -1,5 +1,6 @@
 package com.tong.fpl.domain.data.letletme.player;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -9,6 +10,7 @@ public class PlayerQueryParam {
 
     private int element;
     private int code;
+    @JsonProperty("web_name")
     private String webName;
 
 }
