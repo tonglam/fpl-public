d5ad05e4a54108c47591312db5481e519c1409b0
tong <bluedragon00000@gmail.com>
2020-09-13 23:58:00 +0800

    0913-2

--- src/main/java/com/tong/fpl/task/MatchDayTask.java
+++ src/main/java/com/tong/fpl/task/MatchDayTask.java
@@ -1,12 +1,11 @@
 package com.tong.fpl.task;
 
 import com.tong.fpl.service.IRedisCacheSerive;
-import com.tong.fpl.service.ITableQueryService;
-import com.tong.fpl.service.db.TournamentInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
@@ -14,15 +13,13 @@ import java.time.LocalDateTime;
  * Create by tong on 2020/7/21
  */
 @Slf4j
-//@Component
+@Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class MatchDayTask {
 
     private final IRedisCacheSerive redisCacheSerive;
-    private final ITableQueryService tableQueryService;
-    private final TournamentInfoService tournamentInfoService;
 
-    @Scheduled(cron = "0 0/5 20-23 * * *")
+    @Scheduled(cron = "0 0/5 0-4 * * *")
     public void insertEventLiveCache() {
         log.info("start insertEventLiveCache task, time:{}", LocalDateTime.now());
         int event = this.redisCacheSerive.getCurrentEvent();
