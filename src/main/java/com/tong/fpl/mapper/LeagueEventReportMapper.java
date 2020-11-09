32348336cc4e608d47cad901e8ca31c0e2c9472f
tong <bluedragon00000@gmail.com>
2020-11-09 10:34:38 +0800

    fix bug

--- src/main/java/com/tong/fpl/mapper/LeagueEventReportMapper.java
+++ src/main/java/com/tong/fpl/mapper/LeagueEventReportMapper.java
@@ -2,9 +2,16 @@ package com.tong.fpl.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tong.fpl.domain.entity.LeagueEventReportEntity;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * Create by tong on 2020/11/6
  */
 public interface LeagueEventReportMapper extends BaseMapper<LeagueEventReportEntity> {
+
+	@Select("select DISTINCT(league_name) from league_event_report")
+	List<String> qryLeagueNameList();
+
 }
