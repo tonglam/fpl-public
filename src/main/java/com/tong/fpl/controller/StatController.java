39a05d76bc68ca568fa1bdaafa112a4707a824c3
tong <bluedragon00000@gmail.com>
2020-12-17 10:13:21 +0800

    1217

--- src/main/java/com/tong/fpl/controller/StatController.java
+++ src/main/java/com/tong/fpl/controller/StatController.java
@@ -30,113 +30,116 @@ import java.util.Map;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class StatController {
 
-    private final IStatApi statApi;
-    private final IHttpApi httpApi;
+	private final IStatApi statApi;
+	private final IHttpApi httpApi;
 
-    @GetMapping(value = "/price")
-    public String priceController() {
-        return "stat/price";
-    }
+	@GetMapping(value = "/price")
+	public String priceController() {
+		return "stat/price";
+	}
 
-    @GetMapping(value = "/compare")
-    public String compareController() {
-        return "stat/compare";
-    }
+	@GetMapping(value = "/compare")
+	public String compareController() {
+		return "stat/compare";
+	}
 
-    @GetMapping(value = "/selected")
-    public String selectedController(Model model) {
-        List<String> leagueList = this.statApi.qryTeamSelectStatList();
-        if (!CollectionUtils.isEmpty(leagueList)) {
-            model.addAttribute("leagueList", leagueList);
-        }
-        model.addAttribute("currentGw", this.httpApi.getCurrentEvent());
-        model.addAttribute("gwMap", CommonUtils.createGwMapForOption());
-        return "stat/selected";
-    }
+	@GetMapping(value = "/selected")
+	public String selectedController(Model model) {
+		List<String> leagueList = this.statApi.qryTeamSelectStatList();
+		if (!CollectionUtils.isEmpty(leagueList)) {
+			model.addAttribute("leagueList", leagueList);
+		}
+		model.addAttribute("currentGw", this.httpApi.getCurrentEvent());
+		model.addAttribute("gwMap", CommonUtils.createGwMapForOption());
+		return "stat/selected";
+	}
 
-    @GetMapping(value = "/scout")
-    public String scoutController(Model model, HttpSession session) {
-        int next = this.httpApi.getNextEvent();
-        int entry = 0;
-        if (session.getAttribute("entry") != null) {
-            entry = Integer.parseInt(session.getAttribute("entry").toString());
-        }
-        model.addAttribute("scoutEntryEventData", this.statApi.qryScoutEntryEventData(next, entry));
-        model.addAttribute("nextGw", next);
-        model.addAttribute("fund", RedisUtils.getValueByKey("scoutFund"));
-        model.addAttribute("deadline", this.statApi.getScoutDeadline(next));
-        model.addAttribute("pickPlayerData", this.statApi.qryOffiaccountPickList());
-        return "stat/scout";
-    }
+	@GetMapping(value = "/scout")
+	public String scoutController(Model model, HttpSession session) {
+		int next = this.httpApi.getNextEvent();
+		int entry = 0;
+		if (session.getAttribute("entry") != null) {
+			entry = Integer.parseInt(session.getAttribute("entry").toString());
+		}
+		model.addAttribute("scoutEntryEventData", this.statApi.qryScoutEntryEventData(next, entry));
+		model.addAttribute("nextGw", next);
+		model.addAttribute("fund", RedisUtils.getValueByKey("scoutFund"));
+		model.addAttribute("deadline", this.statApi.getScoutDeadline(next));
+		model.addAttribute("pickPlayerData", this.statApi.qryOffiaccountPickList());
+		return "stat/scout";
+	}
 
-    /**
-     * @apiNote price
-     */
-    @RequestMapping("/qryPriceChangeList")
-    @ResponseBody
-    public TableData<PlayerValueData> qryPriceChangeList() {
-        return this.statApi.qryPriceChangeList();
-    }
+	/**
+	 * @apiNote price
+	 */
+	@RequestMapping("/qryPriceChangeList")
+	@ResponseBody
+	public TableData<PlayerValueData> qryPriceChangeList() {
+		return this.statApi.qryPriceChangeList();
+	}
 
-    /**
-     * @apiNote compare
-     */
-    @RequestMapping("/qryPlayerList")
-    @ResponseBody
-    public TableData<PlayerInfoData> qryPlayerList(@RequestParam String season) {
-        return this.statApi.qryPlayerList(season);
-    }
+	/**
+	 * @apiNote compare
+	 */
+	@RequestMapping("/qryPlayerList")
+	@ResponseBody
+	public TableData<PlayerInfoData> qryPlayerList(@RequestParam String season) {
+		return this.statApi.qryPlayerList(season);
+	}
 
-    /**
-     * @apiNote selected
-     */
-    @RequestMapping("/qryTeamSelectStatByName")
-    @ResponseBody
-    public TableData<LeagueStatData> qryTeamSelectStatByName(@RequestParam String leagueName, @RequestParam int event) {
-        return this.statApi.qryTeamSelectStatByName(leagueName, event);
-    }
+	/**
+	 * @apiNote selected
+	 */
+	@RequestMapping("/qryTeamSelectStatByName")
+	@ResponseBody
+	public TableData<LeagueStatData> qryTeamSelectStatByName(@RequestParam String leagueName, @RequestParam int event) {
+		return this.statApi.qryTeamSelectStatByName(leagueName, event);
+	}
 
-    /**
-     * @apiNote scout
-     */
-    @RequestMapping("/qryScoutPlayerList")
-    @ResponseBody
-    public TableData<ScoutPlayerData> qryScoutPlayerList(@RequestParam int elementType) {
-        return this.statApi.qryScoutPlayerList(elementType);
-    }
+	/**
+	 * @apiNote scout
+	 */
+	@RequestMapping("/qryScoutPlayerList")
+	@ResponseBody
+	public TableData<ScoutPlayerData> qryScoutPlayerList(@RequestParam int elementType) {
+		if (elementType == 0) {
+			return new TableData<>();
+		}
+		return this.statApi.qryScoutPlayerList(elementType);
+	}
 
-    @RequestMapping("/upsertEventScout")
-    @ResponseBody
-    public String upsertEventScout(@RequestBody ScoutData scoutData, HttpSession session) throws Exception {
-        int entry = Integer.parseInt(session.getAttribute("entry").toString());
-        Map<Object, Object> map = RedisUtils.getHashByKey("scoutEntry");
-        if (!map.containsKey(String.valueOf(entry))) {
-            return "请先加入让让群球探！";
-        }
-        scoutData
-                .setEvent(this.httpApi.getNextEvent())
-                .setEntry(entry)
-                .setScoutName((String) RedisUtils.getHashByKey("scoutEntry").get(String.valueOf(entry)));
-        this.statApi.upsertEventScout(scoutData);
-        return "提交成功";
-    }
+	@RequestMapping("/upsertEventScout")
+	@ResponseBody
+	public String upsertEventScout(@RequestBody ScoutData scoutData, HttpSession session) throws Exception {
+		int entry = Integer.parseInt(session.getAttribute("entry").toString());
+		Map<Object, Object> map = RedisUtils.getHashByKey("scoutEntry");
+		if (!map.containsKey(String.valueOf(entry))) {
+			return "请先加入让让群球探！";
+		}
+		scoutData
+				.setEvent(this.httpApi.getNextEvent())
+				.setEntry(entry)
+				.setScoutName((String) RedisUtils.getHashByKey("scoutEntry").get(String.valueOf(entry)));
+		this.statApi.upsertEventScout(scoutData);
+		return "提交成功";
+	}
 
-    @RequestMapping("/qryEventScoutPickList")
-    @ResponseBody
-    public TableData<ScoutData> qryEventScoutPickList(@RequestParam int event) {
-        return this.statApi.qryEventScoutPickList(event);
-    }
+	@RequestMapping("/qryEventScoutPickList")
+	@ResponseBody
+	public TableData<ScoutData> qryEventScoutPickList(@RequestParam int event) {
+		return this.statApi.qryEventScoutPickList(event);
+	}
 
-    @RequestMapping("/qryEventScoutList")
-    @ResponseBody
-    public TableData<ScoutData> qryEventScoutList(@RequestParam int event) {
-        return this.statApi.qryEventScoutList(event);
-    }
+	@RequestMapping("/qryEventScoutList")
+	@ResponseBody
+	public TableData<ScoutData> qryEventScoutList(@RequestParam int event) {
+		return this.statApi.qryEventScoutList(event);
+	}
 
-    @RequestMapping("/getScoutEventList")
-    @ResponseBody
-    public List<DropdownData> getScoutEvent() {
-        return this.statApi.getScoutEvent();
-    }
+	@RequestMapping("/getScoutEventList")
+	@ResponseBody
+	public List<DropdownData> getScoutEvent() {
+		return this.statApi.getScoutEvent();
+	}
 
 }
