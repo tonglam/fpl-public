46ac5c436c46f4c3127ea176d1abb71350e99ccc
tong <bluedragon00000@gmail.com>
2021-02-04 22:36:32 +0800

    0204

---
+++ src/main/java/com/tong/fpl/constant/enums/PlayersAccount.java
@@ -0,0 +1,72 @@
+package com.tong.fpl.constant.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.stream.Stream;
+
+/**
+ * Create by tong on 2022/02/04
+ */
+@Getter
+@AllArgsConstructor
+public enum PlayersAccount {
+
+    McGinn(3651702),
+    Justin(3995410),
+    Bartley(2956964),
+    Hector(4333448),
+    Steele(3949083),
+    Maguire(2824860),
+    Cooper(5630688),
+    Bamford(976717),
+    Webster(3994143),
+    Austin(2317459),
+    Grant(3533650),
+    Robertson(3011137),
+    Maddison(5197871),
+    Ayling(4977097),
+    McCarthy(4431216),
+    Walton(4869402),
+    Hourihane(4667259),
+    Davis(5621264),
+    Alexander_Arnold(1026172),
+    Murphy(4501590),
+    Heaton(3204228),
+    Ramsey(4973262),
+    Philips(5689627),
+    Butland(4627487),
+    Struijk(5664364),
+    Targett(50383),
+    Reed(5657419),
+    Berge(5091389),
+    Reid(5639035),
+    Albrighton(6523017),
+    Stephens(2073952),
+    Snodgrass(3560071),
+    Alioski(6297033),
+    Townsend(1605584),
+    Taylorv(3257654),
+    Choudhury(6824049),
+    Poveda(5677809),
+    Jahanbakhsh(5797199),
+    Bissouma(5747167),
+    Bendtner(3349042),
+    Crouch(5100073),
+    Richards(113765);
+
+    private final int entry;
+
+    public static int getPlayerAccountEntry(String name) {
+        if (name.contains("-")) {
+            return getPlayerAccountEntry(name.replaceAll("-", "_"));
+        }
+        return Stream.of(PlayersAccount.values())
+                .filter(o -> StringUtils.equals(o.name(), name))
+                .map(PlayersAccount::getEntry)
+                .findFirst()
+                .orElse(0);
+    }
+
+}
