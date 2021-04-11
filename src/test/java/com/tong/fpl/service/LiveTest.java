fba87e454c56b7e16eaafa0a7c091f43b1a942d3
tong <bluedragon00000@gmail.com>
2021-04-12 00:20:34 +0800

    fix live

--- src/test/java/com/tong/fpl/service/LiveTest.java
+++ src/test/java/com/tong/fpl/service/LiveTest.java
@@ -19,7 +19,7 @@ public class LiveTest extends FplApplicationTests {
 	private ILiveService liveCalcService;
 
 	@ParameterizedTest
-	@CsvSource({"31, 413107"})
+	@CsvSource({"31, 418989"})
 	void calcLivePoints(int event, int entry) {
 		long start = System.currentTimeMillis();
 		LiveCalcData liveCalaData = this.liveCalcService.calcLivePointsByEntry(event, entry);
